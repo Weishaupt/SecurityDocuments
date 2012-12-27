a2fa046301876183a6954413970c6928ba349ead
Kjell Braden <afflux@pentabarf.de>
2012-12-27 14:10:42 +0100

    adjust CryptOracleTest to current API

--- CryptOracleTest/src/de/pentabarf/cryptoracletest/MainActivity.java
+++ CryptOracleTest/src/de/pentabarf/cryptoracletest/MainActivity.java
@@ -1,5 +1,9 @@
 package de.pentabarf.cryptoracletest;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.Reader;
+import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
 import java.security.Key;
@@ -14,14 +18,23 @@ import java.security.Provider;
 import java.security.PublicKey;
 import java.security.Security;
 import java.security.Signature;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.text.MessageFormat;
+import java.util.Date;
+import java.util.List;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.security.auth.x500.X500Principal;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -42,8 +55,50 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({ "unused" })
 public class MainActivity extends Activity {
+    private static final String testcert = "-----BEGIN CERTIFICATE-----\n"
+	    + "MIIHWTCCBUGgAwIBAgIDCkGKMA0GCSqGSIb3DQEBCwUAMHkxEDAOBgNVBAoTB1Jv\n"
+	    + "b3QgQ0ExHjAcBgNVBAsTFWh0dHA6Ly93d3cuY2FjZXJ0Lm9yZzEiMCAGA1UEAxMZ\n"
+	    + "Q0EgQ2VydCBTaWduaW5nIEF1dGhvcml0eTEhMB8GCSqGSIb3DQEJARYSc3VwcG9y\n"
+	    + "dEBjYWNlcnQub3JnMB4XDTExMDUyMzE3NDgwMloXDTIxMDUyMDE3NDgwMlowVDEU\n"
+	    + "MBIGA1UEChMLQ0FjZXJ0IEluYy4xHjAcBgNVBAsTFWh0dHA6Ly93d3cuQ0FjZXJ0\n"
+	    + "Lm9yZzEcMBoGA1UEAxMTQ0FjZXJ0IENsYXNzIDMgUm9vdDCCAiIwDQYJKoZIhvcN\n"
+	    + "AQEBBQADggIPADCCAgoCggIBAKtJNRFIfNImflOUz0Op3SjXQiqL84d4GVh8D57a\n"
+	    + "iX3h++tykA10oZZkq5+gJJlz2uJVdscXe/UErEa4w75/ZI0QbCTzYZzA8pD6Ueb1\n"
+	    + "aQFjww9W4kpCz+JEjCUoqMV5CX1GuYrz6fM0KQhF5Byfy5QEHIGoFLOYZcRD7E6C\n"
+	    + "jQnRvapbjZLQ7N6QxX8KwuPr5jFaXnQ+lzNZ6MMDPWAzv/fRb0fEze5ig1JuLgia\n"
+	    + "pNkVGJGmhZJHsK5I6223IeyFGmhyNav/8BBdwPSUp2rVO5J+TJAFfpPBLIukjmJ0\n"
+	    + "FXFuC3ED6q8VOJrU0gVyb4z5K+taciX5OUbjchs+BMNkJyIQKopPWKcDrb60LhPt\n"
+	    + "XapI19V91Cp7XPpGBFDkzA5CW4zt2/LP/JaT4NsRNlRiNDiPDGCbO5dWOK3z0luL\n"
+	    + "oFvqTpa4fNfVoIZwQNORKbeiPK31jLvPGpKK5DR7wNhsX+kKwsOnIJpa3yxdUly6\n"
+	    + "R9Wb7yQocDggL9V/KcCyQQNokszgnMyXS0XvOhAKq3A6mJVwrTWx6oUrpByAITGp\n"
+	    + "rmB6gCZIALgBwJNjVSKRPFbnr9s6JfOPMVTqJouBWfmh0VMRxXudA/Z0EeBtsSw/\n"
+	    + "LIaRmXGapneLNGDRFLQsrJ2vjBDTn8Rq+G8T/HNZ92ZCdB6K4/jc0m+YnMtHmJVA\n"
+	    + "BfvpAgMBAAGjggINMIICCTAdBgNVHQ4EFgQUdahxYEyIE/B42Yl3tW3Fid+8sXow\n"
+	    + "gaMGA1UdIwSBmzCBmIAUFrUyG9TH8+DmjvO90rA67rI5GNGhfaR7MHkxEDAOBgNV\n"
+	    + "BAoTB1Jvb3QgQ0ExHjAcBgNVBAsTFWh0dHA6Ly93d3cuY2FjZXJ0Lm9yZzEiMCAG\n"
+	    + "A1UEAxMZQ0EgQ2VydCBTaWduaW5nIEF1dGhvcml0eTEhMB8GCSqGSIb3DQEJARYS\n"
+	    + "c3VwcG9ydEBjYWNlcnQub3JnggEAMA8GA1UdEwEB/wQFMAMBAf8wXQYIKwYBBQUH\n"
+	    + "AQEEUTBPMCMGCCsGAQUFBzABhhdodHRwOi8vb2NzcC5DQWNlcnQub3JnLzAoBggr\n"
+	    + "BgEFBQcwAoYcaHR0cDovL3d3dy5DQWNlcnQub3JnL2NhLmNydDBKBgNVHSAEQzBB\n"
+	    + "MD8GCCsGAQQBgZBKMDMwMQYIKwYBBQUHAgEWJWh0dHA6Ly93d3cuQ0FjZXJ0Lm9y\n"
+	    + "Zy9pbmRleC5waHA/aWQ9MTAwNAYJYIZIAYb4QgEIBCcWJWh0dHA6Ly93d3cuQ0Fj\n"
+	    + "ZXJ0Lm9yZy9pbmRleC5waHA/aWQ9MTAwUAYJYIZIAYb4QgENBEMWQVRvIGdldCB5\n"
+	    + "b3VyIG93biBjZXJ0aWZpY2F0ZSBmb3IgRlJFRSwgZ28gdG8gaHR0cDovL3d3dy5D\n"
+	    + "QWNlcnQub3JnMA0GCSqGSIb3DQEBCwUAA4ICAQApKIWuRKm5r6R5E/CooyuXYPNc\n"
+	    + "7uMvwfbiZqARrjY3OnYVBFPqQvX56sAV2KaC2eRhrnILKVyQQ+hBsuF32wITRHhH\n"
+	    + "Va9Y/MyY9kW50SD42CEH/m2qc9SzxgfpCYXMO/K2viwcJdVxjDm1Luq+GIG6sJO4\n"
+	    + "D+Pm1yaMMVpyA4RS5qb1MyJFCsgLDYq4Nm+QCaGrvdfVTi5xotSu+qdUK+s1jVq3\n"
+	    + "VIgv7nSf7UgWyg1I0JTTrKSi9iTfkuO960NAkW4cGI5WtIIS86mTn9S8nK2cde5a\n"
+	    + "lxuV53QtHA+wLJef+6kzOXrnAzqSjiL2jA3k2X4Ndhj3AfnvlpaiVXPAPHG0HRpW\n"
+	    + "Q7fDCo1y/OIQCQtBzoyUoPkD/XFzS4pXM+WOdH4VAQDmzEoc53+VGS3FpQyLu7Xt\n"
+	    + "hbNc09+4ufLKxw0BFKxwWMWMjTPUnWajGlCVI/xI4AZDEtnNp4Y5LzZyo4AQ5OHz\n"
+	    + "0ctbGsDkgJp8E3MGT9ujayQKurMcvEp4u+XjdTilSKeiHq921F73OIZWWonO1sOn\n"
+	    + "ebJSoMbxhbQljPI/lrMQ2Y1sVzufb4Y6GIIiNsiwkTjbKqGTqoQ/9SdlrnPVyNXT\n"
+	    + "d+pLncdBu8fA46A/5H2kjXPmEkvfoXNzczqA6NXLji/L6hOn1kGLrPo8idck9U60\n"
+	    + "4GGSt/M3mMS+lqO3ig==\n" + "-----END CERTIFICATE-----\n";
+
     protected static final String TAG = MainActivity.class.getSimpleName();
 
     protected String selectedAlias;
@@ -65,16 +120,87 @@ public class MainActivity extends Activity {
 	this.data = null;
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//	// Inflate the menu; this adds items to the action bar if it is present.
-//	getMenuInflater().inflate(R.menu.activity_main, menu);
-//	return true;
-//    }
-
     public void importClicked(View view) {
 	Intent i = KeyChain.createInstallIntent();
 	startActivity(i);
+    }
+
+    public static X509Certificate convertFromPem(String data) {
+	try {
+	    CertificateFactory certFactory = CertificateFactory
+		    .getInstance("X.509");
+	    Certificate cert = certFactory
+		    .generateCertificate(new ByteArrayInputStream(data
+			    .getBytes()));
+	    return (X509Certificate) cert;
+	} catch (CertificateException e) {
+	    throw new AssertionError(e);
+	}
+    }
+
+    public void accessPublicClicked(View view) {
+	new AsyncTask<Void, String, X509Certificate>() {
+	    ProgressDialog pd;
+
+	    private String ptxt = "foobarbaz";
+
+	    protected void onPreExecute() {
+
+		this.pd = new ProgressDialog(MainActivity.this);
+		this.pd.setMessage("Loading...");
+		this.pd.setIndeterminate(true);
+		this.pd.setCancelable(false);
+		this.pd.show();
+	    };
+
+	    @Override
+	    protected X509Certificate doInBackground(Void... params) {
+		try {
+		    X509Certificate cert = convertFromPem(testcert);
+
+		    Log.i(TAG, "imported cert=" + cert.toString());
+
+		    CryptOracle.storePublicCertificate(MainActivity.this,
+			    "testimport", cert);
+
+		    return KeyChain.getCertificateChain(MainActivity.this,
+			    "testimport")[0];
+		} catch (Throwable e) {
+		    Log.e(TAG, "exception while testing import/export", e);
+		    e.printStackTrace();
+		    return null;
+		}
+	    }
+
+	    protected void onProgressUpdate(final String... progress) {
+
+		Log.e(TAG, "progress: " + progress[0]);
+		runOnUiThread(new Runnable() {
+
+		    @SuppressWarnings("unqualified-field-access")
+		    @Override
+		    public void run() {
+			pd.setMessage(progress[0]);
+		    }
+		});
+	    }
+
+	    protected void onPostExecute(X509Certificate result) {
+		this.pd.dismiss();
+
+		if (result == null)
+		    return;
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(
+			MainActivity.this);
+
+		builder.setMessage("exported cert=" + result.toString())
+			.setTitle("Result");
+
+		AlertDialog dialog = builder.create();
+		dialog.show();
+	    };
+	}.execute();
     }
 
     public void testClicked(View view) {
@@ -112,7 +238,7 @@ public class MainActivity extends Activity {
 					    .getCertificateChain(
 						    MainActivity.this, keyAlias)[0]
 					    .getPublicKey();
-				    
+
 				    Log.i(TAG, "puk=" + puk.toString());
 				    Log.i(TAG, "prk=" + prk.toString());
 
@@ -124,8 +250,8 @@ public class MainActivity extends Activity {
 				    byte[] sigData = sig.sign();
 
 				    publishProgress("verifying...");
-				    sig = Signature.getInstance(
-					    "SHA1withRSA", "AndroidOpenSSL");
+				    sig = Signature.getInstance("SHA1withRSA",
+					    "AndroidOpenSSL");
 				    sig.initVerify(puk);
 				    sig.update(this.ptxt.getBytes());
 				    if (!sig.verify(sigData))
@@ -246,12 +372,13 @@ public class MainActivity extends Activity {
 	    throw new IllegalArgumentException("key unusable - algorithm \""
 		    + key.getAlgorithm() + "\" not supported");
 	} catch (NoSuchProviderException e) {
-	    throw new IllegalArgumentException("android openssl provider not found");
+	    throw new IllegalArgumentException(
+		    "android openssl provider not found");
 	}
     }
 
     public void generateClicked(View view) {
-	CryptOracle.generate(this, new KeyChainAliasCallback() {
+	CryptOracle.requestKey(this, new KeyChainAliasCallback() {
 
 	    @Override
 	    public void alias(String alias) {
@@ -304,6 +431,93 @@ public class MainActivity extends Activity {
 		    protected void onPostExecute(byte[] result) {
 			this.pd.dismiss();
 			MainActivity.this.data = result;
+		    };
+		}.execute(response);
+
+	    }
+	});
+    }
+
+    public void signClicked(View view) {
+	if (this.selectedAlias == null)
+	    return;
+
+	prompt("sign", "provide data:", new ResponseHandler<String>() {
+	    @Override
+	    public void handle(String response) {
+
+		new AsyncTask<String, Void, byte[]>() {
+
+		    private ProgressDialog pd;
+
+		    protected void onPreExecute() {
+			this.pd = new ProgressDialog(MainActivity.this);
+			this.pd.setMessage("Signing...");
+			this.pd.setIndeterminate(true);
+			this.pd.setCancelable(false);
+			this.pd.show();
+		    };
+
+		    @Override
+		    protected byte[] doInBackground(String... params) {
+			try {
+			    return CryptOracle.sign(MainActivity.this,
+				    MainActivity.this.selectedAlias,
+				    "SHA1withRSA", params[0].getBytes());
+			} catch (Exception e) {
+			    Log.e(TAG, "error while signing data:", e);
+			    return null;
+			}
+		    }
+
+		    protected void onPostExecute(byte[] result) {
+			this.pd.dismiss();
+			MainActivity.this.data = result;
+		    };
+		}.execute(response);
+
+	    }
+	});
+    }
+
+    public void verifyClicked(View view) {
+	if (this.selectedAlias == null)
+	    return;
+
+	prompt("verify", "provide data:", new ResponseHandler<String>() {
+	    @Override
+	    public void handle(String response) {
+
+		new AsyncTask<String, Void, Boolean>() {
+
+		    private ProgressDialog pd;
+
+		    protected void onPreExecute() {
+			this.pd = new ProgressDialog(MainActivity.this);
+			this.pd.setMessage("Verifying...");
+			this.pd.setIndeterminate(true);
+			this.pd.setCancelable(false);
+			this.pd.show();
+		    };
+
+		    @Override
+		    protected Boolean doInBackground(String... params) {
+			try {
+			    return CryptOracle.verify(MainActivity.this,
+				    MainActivity.this.selectedAlias,
+				    "SHA1withRSA", params[0].getBytes(),
+				    MainActivity.this.data);
+			} catch (Exception e) {
+			    Log.e(TAG, "error while verifying data:", e);
+			    return null;
+			}
+		    }
+
+		    protected void onPostExecute(Boolean result) {
+			this.pd.dismiss();
+			Toast.makeText(MainActivity.this,
+				result ? "good signature" : "bad signature",
+				Toast.LENGTH_LONG).show();
 		    };
 		}.execute(response);
 
